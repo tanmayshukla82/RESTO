e14b1a027bb6f3a65461183e451d08c4f4a936f2
tanmayshukla82 <67494383+tanmayshukla82@users.noreply.github.com>
2021-09-17 14:03:18 +0530

    commit-2

--- server.js
+++ server.js
@@ -6,11 +6,29 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+
+//assets setting
+app.use(express.static(__dirname+'/public'));
+
 //template engine
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs');
 
+app.get("/",(req,res)=>{
+    res.render('home');
+})
+
+app.get("/cart",(req,res)=>{
+    res.render('customers/cart');
+})
+
+app.get("/login",(req,res)=>{
+    res.render('auth/login');
+})
+app.get("/register",(req,res)=>{
+    res.render('auth/register');
+})
 app.listen(PORT,()=>{
     console.log(`connection successful to PORT = ${PORT}`);
 });
